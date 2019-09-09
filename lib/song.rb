55dcf23945c149f597cfd55ec4dd4b4a341ba391
Stephen Blais <stephen@s-blais.com>
2019-09-09 20:22:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,9 +39,9 @@ class Song
     genre.add_song(self)
   end
 
-  def self.find_by_name(name)
-    self.all.find {|song| song.name == name}
-  end
+  # def self.find_by_name(name)
+  #   self.all.find {|song| song.name == name}
+  # end
 
   def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create(name)
