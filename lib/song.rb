89f14cb14e646b105234bbfc7d6fdd06626a8bc3
Stephen Blais <stephen@s-blais.com>
2019-09-09 20:52:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
   #   self.find_by_name(name) || self.create(name)
   # end
 
-  def new_from_filename(filename)
+  def self.new_from_filename(filename)
 
     artist = filename.split(" - ")[0]
     name = (filename.split(" - ")[1])
