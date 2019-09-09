a585ca63b1c8bc7de95ee519855a08ea53e47447
Stephen Blais <stephen@s-blais.com>
2019-09-09 22:12:53 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -36,7 +36,7 @@ class MusicLibraryController
     list = Song.all
     list.sort {|a,b| a.name <=> b.name}
     list.each do |song|
-      puts "#{counter}. #{song}"
+      puts "#{counter}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
 
 
