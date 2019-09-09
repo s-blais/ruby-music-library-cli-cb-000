7c766f7d4a644f8edf43ba47fb17f04bcfea3c2c
Stephen Blais <stephen@s-blais.com>
2019-09-09 20:27:15 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -4,5 +4,9 @@ module Concerns::Findable
     self.all.find {|instance| instance.name == name}
   end
 
+  def find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
+
 
 end
