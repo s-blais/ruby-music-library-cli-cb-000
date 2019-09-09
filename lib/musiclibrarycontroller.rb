24371d3335bb7399b1087b2edb9f3dee31893247
Stephen Blais <stephen@s-blais.com>
2019-09-09 21:37:44 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -8,8 +8,8 @@ class MusicLibraryController
   end
 
   def call
-    input = nil
-    until input = "exit"
+    # input = nil
+    # until input = "exit"
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
@@ -20,7 +20,7 @@ class MusicLibraryController
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
       input = gets.strip
-    end
+    # end
   end
 
 
